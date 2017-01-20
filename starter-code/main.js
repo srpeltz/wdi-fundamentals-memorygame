c7a015dc69278ee0bd501e8a148c29573ffd9e0f
Steven Peltzer <speltzer@Stevens-MacBook-Pro.local>
2017-01-19 17:20:41 -0800

    Additional Game Logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,9 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne == cardTwo) {
+	alert = "You found a match!";
+} else {
+	alert = "Sorry, try again";
+}
