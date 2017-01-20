346ecb374e694ee7f15bb99844c3d07b3d966ac6
Steven Peltzer <speltzer@Stevens-MacBook-Pro.local>
2017-01-20 00:49:08 -0800

    Almost Final

--- starter-code/main.js
+++ starter-code/main.js
@@ -17,6 +17,7 @@ var cardFour = "king";
 // }
 
 var document.getElementById('game-board');
+
 function createBoard() {
 	for (var i=0; i<cards.length; i++){
 		var cardElement = document.createElement('div');
@@ -24,4 +25,31 @@ function createBoard() {
 		board.appendChild(cardElement);
 	}
 }
-function createCards
+
+
+var cards = ['queen', 'queen', 'king', 'king',];
+var cardsInPlay = []; 
+for (var i=0; i<cards.length; i++) 
+{
+	cardElement.setAttribute('data-card', cards[i]);
+	cardElement.addEventListener('click', isTwoCards)
+}
+
+function isMatch(cards) {
+	if (cards[0] === cards[1])
+	{
+		alert("You found a match!");
+	}
+	else {
+		alert("Try Again.");
+	}
+}
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay.length === 2){
+		isMatch(cardsInPlay);
+	}
+	else (cardsInPlay = [];)
+};
+
