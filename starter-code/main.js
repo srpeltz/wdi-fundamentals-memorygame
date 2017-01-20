8cd6a26206679cbd9e913a8fb4b7c350c2fa90cf
Steven Peltzer <speltzer@Stevens-MacBook-Pro.local>
2017-01-19 22:27:37 -0800

    javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,8 +4,24 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne == cardTwo) {
-	alert = "You found a match!";
-} else {
-	alert = "Sorry, try again";
+// if (cardOne == cardTwo) {
+// 	alert = "You found a match!";
+// } else {
+// 	alert = "Sorry, try again";
+// }
+
+// if (cardThree == cardFour) {
+// 	alert = "You found a match!";
+// } else {
+// 	alert = "Sorry, try again";
+// }
+
+var document.getElementById('game-board');
+function createBoard() {
+	for (var i=0; i<cards.length; i++){
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+	}
 }
+function createCards
