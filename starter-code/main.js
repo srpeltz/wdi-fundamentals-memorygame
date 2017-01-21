962ef6a911d9bc9dd4797293e305e5e99df24a16
steven peltzer <srpeltzer@yahoo.com>
2017-01-21 11:49:59 -0800

    final submission

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,8 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
 // if (cardOne == cardTwo) {
 // 	alert = "You found a match!";
@@ -16,23 +16,43 @@ var cardFour = "king";
 // 	alert = "Sorry, try again";
 // }
 
-var document.getElementById('game-board');
 
+var cards = ['queen', 'queen', 'king', 'king',];
+
+var cardsInPlay = []; 
+
+
+var board = document.getElementById('game-board');
 function createBoard() {
 	for (var i=0; i<cards.length; i++){
 		var cardElement = document.createElement('div');
 		cardElement.className = 'card';
+
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+		
+		board.appendChild(cardElement);
 		board.appendChild(cardElement);
 	}
 }
 
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') 
+	{
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>";
+	}
+	else 
+	{
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>";
+	}
+ 
+	if (cardsInPlay.length === 2){
+		isMatch(cardsInPlay);
 
-var cards = ['queen', 'queen', 'king', 'king',];
-var cardsInPlay = []; 
-for (var i=0; i<cards.length; i++) 
-{
-	cardElement.setAttribute('data-card', cards[i]);
-	cardElement.addEventListener('click', isTwoCards)
+		cardsInPlay = [];
+}
 }
 
 function isMatch(cards) {
@@ -40,16 +60,12 @@ function isMatch(cards) {
 	{
 		alert("You found a match!");
 	}
-	else {
+	else 
+	{
 		alert("Try Again.");
 	}
 }
 
-function isTwoCards() {
-	cardsInPlay.push(this.getAttribute('data-card'));
-	if (cardsInPlay.length === 2){
-		isMatch(cardsInPlay);
-	}
-	else (cardsInPlay = [];)
-};
+createBoard();
+
 
